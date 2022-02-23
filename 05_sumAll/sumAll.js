9ee244e48326d91dbbea8ffa0fc6f2ab887fe9c4
Betsue Weldemariam <btsegay0@gmail.com>
2022-02-23 10:19:10 +0300

    Completed up to 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -6,12 +6,12 @@ const sumAll = function(num1, num2) {
 
     else if (typeof(num1) !== 'number' || typeof(num2) !== 'number') return 'ERROR';
 
-    else if ((num1 > num2) && (num1 > 0 && num2 > 0)) {
+    else if ((num1 > num2)) {
         for (let i = num2; i <= num1; i++) { // 1 2 3 4
             sum += i;
         }
     }
-    else if ((num1 < num2 || num1 == num2) && (num1 > 0 && num2 > 0)){
+    else if ((num1 < num2 || num1 == num2)){
         for (let i = num1; i <= num2; i++) {
             sum += i;
         }
